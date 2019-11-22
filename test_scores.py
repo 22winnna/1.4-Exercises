eb5f47dae40c3c58dfb606ac8e7250cd124ff834
22winnna <49483246+22winnna@users.noreply.github.com>
2019-11-22 10:53:47 -0500

    1.4 exercises
    These are the 1.4 exercises.

---
+++ test_scores.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+def display_welcome():
+    print("The Test Scores program")
+    print("Enter 'x' to exit")
+    print("")
+
+def get_scores():
+    scores = []
+    while True:
+        score = input("Enter test score: ")
+        if score == "x":
+            return  scores
+        else:
+            score = int(score)
+            if score >= 0 and score <= 100:
+                scores.append(score)
+            else:
+                print("Test score must be from 0 through 100. " +
+                      "Score discarded. Try again.")
+
+def process_scores(scores):
+    total_scores = 0
+    for score in scores:
+        total_scores += score # total_scores = total_scores + score
+    average = total_scores / len(scores)
+    median_index = len(scores) // 2
+    median = scores[median_index]
+         
+                
+    # format and display the result
+    print()
+    print("Score total:       ", total_scores)
+    print("Number of Scores:  ", len(scores))
+    print("Average Score:     ", average)
+    print("Low Score:         ", min(scores))
+    print("High Score:        ", max(scores))
+    print("Median Score:      ", median)
+
+def main():
+    display_welcome()
+    scores = get_scores()
+    process_scores(scores)
+    print("")
+    print("Bye!")
+
+# if started as the main module, call the main function
+if __name__ == "__main__":
+    main()
